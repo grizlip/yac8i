6b2c4cc55dbaa2473c8b5d68d483a7d9204b7a81
grizlip <42873709+grizlip@users.noreply.github.com>
2024-06-05 12:14:20 +0200

    Dependency fix for debug

--- yac8i.gui.sdl/Program.cs
+++ yac8i.gui.sdl/Program.cs
@@ -6,6 +6,7 @@ namespace yac8i.gui.sdl
     public class Program
     {
 
+        public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>().UsePlatformDetect();
         // Initialization code. Don't use any Avalonia, third-party APIs or any
         // SynchronizationContext-reliant code before AppMain is called: things aren't initialized
         // yet and stuff might break.
@@ -14,6 +15,5 @@ namespace yac8i.gui.sdl
         {
            BuildAvaloniaApp().StartWithClassicDesktopLifetime(args);
         }
-        public static AppBuilder BuildAvaloniaApp() => AppBuilder.Configure<App>().UsePlatformDetect().LogToTrace();
     }
 }
