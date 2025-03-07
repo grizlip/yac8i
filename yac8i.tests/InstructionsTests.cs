860d630bade9d4ccb2b52275be589dca52bc724f
grizlip <42873709+grizlip@users.noreply.github.com>
2025-03-07 21:41:01 +0100

    Added simple unit test to check if ci works

---
+++ yac8i.tests/InstructionsTests.cs
@@ -0,0 +1,42 @@
+using NUnit.Framework;
+
+namespace yac8i.tests
+{
+    public class InstructionsTests
+    {
+        [Test]
+        public void TestX()
+        {
+            byte result = Instruction.X(0xABCD);
+            Assert.That(result, Is.EqualTo(0x0B));
+        }
+
+        [Test]
+        public void TestY()
+        {
+            byte result = Instruction.Y(0xABCD);
+            Assert.That(result, Is.EqualTo(0x0C));
+        }
+
+        [Test]
+        public void TestN()
+        {
+            byte result = Instruction.N(0xABCD);
+            Assert.That(result, Is.EqualTo(0x0D));
+        }
+
+        [Test]
+        public void TestNN()
+        {
+            byte result = Instruction.NN(0xABCD);
+            Assert.That(result, Is.EqualTo(0xCD));
+        }
+
+        [Test]
+        public void TestNNN()
+        {
+            ushort result = Instruction.NNN(0xABCD);
+            Assert.That(result, Is.EqualTo(0x0BCD));
+        }
+    }
+}
