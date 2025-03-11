f25e53ea58d6887b817fa9a71508cdb8db7015f7
grizlip <42873709+grizlip@users.noreply.github.com>
2025-03-11 21:16:02 +0100

    Code formatting

--- yac8i.tests/InstructionsTests.cs
+++ yac8i.tests/InstructionsTests.cs
@@ -12,7 +12,7 @@ namespace yac8i.tests
         {
             Chip8VM vm = new();
             vm.Surface[0, 0] = true;
-            bool shouldIncrementPC = ExecuteSingleInstruction(vm, 0x00E0,0);
+            bool shouldIncrementPC = ExecuteSingleInstruction(vm, 0x00E0, 0);
             using (Assert.EnterMultipleScope())
             {
                 Assert.That(vm.Surface[0, 0], Is.EqualTo(false));
@@ -27,7 +27,7 @@ namespace yac8i.tests
             Assert.Throws<ArgumentException>(
                 delegate
                 {
-                     ExecuteSingleInstruction(vm, 0x00EE,100);
+                    ExecuteSingleInstruction(vm, 0x00EE, 100);
                 });
         }
 
@@ -36,7 +36,7 @@ namespace yac8i.tests
         {
             Chip8VM vm = new();
             vm.stack.Push(0xFFFF);
-            bool shouldIncrementPC = ExecuteSingleInstruction(vm, 0x00EE,100);
+            bool shouldIncrementPC = ExecuteSingleInstruction(vm, 0x00EE, 100);
             using (Assert.EnterMultipleScope())
             {
                 Assert.That(vm.ProgramCounter, Is.EqualTo(0xFFFF));
@@ -48,7 +48,7 @@ namespace yac8i.tests
         public void TestJP()
         {
             Chip8VM vm = new();
-            bool shouldIncrementPC =ExecuteSingleInstruction(vm, 0x1000,0xFFFF);
+            bool shouldIncrementPC = ExecuteSingleInstruction(vm, 0x1000, 0xFFFF);
             using (Assert.EnterMultipleScope())
             {
                 Assert.That(vm.ProgramCounter, Is.EqualTo(0x0FFF));
@@ -60,7 +60,7 @@ namespace yac8i.tests
         public void TestCALL()
         {
             Chip8VM vm = new();
-            bool shouldIncrementPC =ExecuteSingleInstruction(vm, 0x2000,0xFFFF);
+            bool shouldIncrementPC = ExecuteSingleInstruction(vm, 0x2000, 0xFFFF);
             using (Assert.EnterMultipleScope())
             {
                 Assert.That(vm.ProgramCounter, Is.EqualTo(0x0FFF));
