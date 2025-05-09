b3028c4997a8e5138c26ece30b7c5ae4c352ed86
grizlip <42873709+grizlip@users.noreply.github.com>
2025-05-09 21:14:16 +0200

    Added passing passing information about pressed keys to the VM.

--- yac8i.blazorwasm/Pages/Home.razor.cs
+++ yac8i.blazorwasm/Pages/Home.razor.cs
@@ -8,6 +8,26 @@ namespace yac8i.blazorwasm.Pages
     [System.Runtime.Versioning.SupportedOSPlatform("browser")]
     public partial class Home : IDisposable
     {
+        private static readonly Dictionary<string, ushort> keysMapping = new()
+            {
+                {"1",0x1},
+                {"2",0x2},
+                {"3",0x3},
+                {"4",0xC},
+                {"q",0x4},
+                {"w",0x5},
+                {"e",0x6},
+                {"r",0xD},
+                {"a",0x7},
+                {"s",0x8},
+                {"d",0x9},
+                {"f",0xE},
+                {"z",0xA},
+                {"x",0x0},
+                {"c",0xB},
+                {"v",0xF},
+            };
+
         private readonly byte[] surface = new byte[64 * 32 * 4];//8192
         private readonly JsTickTimer jsTickTimer;
         private readonly Chip8VM vm;
@@ -56,22 +76,28 @@ namespace yac8i.blazorwasm.Pages
         }
 
         [JSInvokable]
-        public void OnKeyDown(string  arg)
+        public void OnKeyDown(string arg)
         {
-            Console.WriteLine(arg);
-            
+            if (keysMapping.TryGetValue(arg, out ushort key))
+            {
+                vm.UpdateKeyState(key, true);
+            }
         }
 
         [JSInvokable]
         public void OnKeyUp(string arg)
         {
-            Console.WriteLine(arg);
+            if (keysMapping.TryGetValue(arg, out ushort key))
+            {
+                vm.UpdateKeyState(key, false);
+            }
         }
 
         public void Dispose()
         {
             objRef?.Dispose();
         }
+        
         protected override async Task OnAfterRenderAsync(bool firstRender)
         {
             if (firstRender)
